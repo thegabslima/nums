183cbbd65eb8b419064f963084683290001e4d3c
Gabriel Lima <gabriel_alvesdelima@yahoo.com.br>
2024-03-01 01:10:08 -0300

    add final configs for ts and jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['tests/*.spec.ts'],
+  testMatch: ['<rootDir>/tests/*.spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
